6b58e2553eb8863ea8f9d6fd499727b8276df06f
Kostofrees <147641301+Kostofrees@users.noreply.github.com>
2023-11-12 00:06:00 +0100

    Actualización diferenciar variable x e y al seleccionarlas en la interfaz
    La variable seleccionada de las checkboxes será la x y la de los buttons la y

--- app.py
+++ app.py
@@ -109,17 +109,19 @@ class Ventana_Principal(Ventana):
         for label in result_labels:
             label.pack()
 
-    def generar_RR(self, vars, indp):
-        cnt = sum(status.get() for _, status in vars)
+    def generar_RR(self, vars, indp):    #vars valores x e indp valor y
+        cntx = sum(status.get() for _, status in vars)
+        
 
-        if cnt != 2:
-            Ventana_Error("Debe seleccionar exactamente 2 variables")
+        if cntx != 1 :
+            Ventana_Error("Debe seleccionar exactamente 1 variable") #hay que cambiarlo para multiple
         else:
-            #estamos cogiendo siempre como var X la que esté más arriba en el orden de seleccion
             self.variables = [var_name for var_name, status in vars if status.get() == 1]
-            m, corte_x, corte_y, ec_recta, r_squared, mse, mae = regresion_lineal(self.data, self.variables[0], self.variables[1])
 
-            self.et_variables.config(text=f"\nDatos: \nVariable X: {self.variables[0]}, Variable Y: {self.variables[1]}")
+
+            m, corte_x, corte_y, ec_recta, r_squared, mse, mae = regresion_lineal(self.data, self.variables[0], indp)
+
+            self.et_variables.config(text=f"\nDatos: \nVariable X: {self.variables[0]}, Variable Y: {indp}")
             self.et_recta.config(text=f"Ecuación recta: {ec_recta}, Pendiente: {m}")
             self.et_cortes.config(text=f"Corte eje X: {corte_x}, Corte eje Y: {corte_y}")
             self.et_errores.config(text=f"Error R^2: {r_squared}, Error cuadrático medio: {mse}, Error absoluto medio: {mae}")
@@ -133,6 +135,11 @@ class Ventana_Principal(Ventana):
         button_S.pack(side="left", padx=10)
 
         button_L = tkinter.Button(frame_abajo, text="Cargar modelo", bg="light grey",
+                                width=17, height=4)
+        button_L.pack(side="right", padx=10)
+
+if __name__ == "__main__":
+    Ventana_Principal()
                                 width=17, height=4)
         button_L.pack(side="right", padx=10)
 
