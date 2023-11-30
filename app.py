8dfc25af82df0c1d327ceff6aa0b2bc4b24a74f5
Hugo Ramos <hugord2013@gmail.com>
2023-11-30 14:36:07 +0100

    modularidad-escoger_archivo

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import tkinter
 from tkinter import ttk, filedialog
-
-
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 """nuestros archivos"""
@@ -11,6 +9,7 @@ from regresion_lineal import *
 from leer_archivos import *
 from guardar_cargar_archivos import *
 from generarRR_ventana import *
+from escoger_arch_ventana import *
 
 class Ventana_Principal(Ventana):
     def __init__(self):
@@ -21,52 +20,9 @@ class Ventana_Principal(Ventana):
         self.variables = []    # Lista para almacenar las variables seleccionadas
         self.et_variables = tkinter.Label(self.frame_var, text="")
 
-        self.escoger_archivo()  # Crear el primer frame
+        escoger_archivo(self)  # Crear el primer frame
         self._save_load()       # Agregar botones de carga/guardado
         self.ventana.mainloop() # Iniciar la ventana
-
-    def escoger_archivo(self):
-        # Crear el primer frame de la ventana
-        self.frame_base = tkinter.Frame(self.ventana)
-        self.frame_base.pack()
-
-        self.boton_escoger = tkinter.Button(self.frame_base, text="Escoger archivo",
-                                            command=self.__escoger_archivo,
-                                            bg="light grey", width=17, height=0)
-        
-        self.boton_escoger.pack(side=tkinter.LEFT, padx=5, pady=15)
-
-        self.et_path = tkinter.Label(self.frame_base)
-        self.et_path.pack()
-        
-
-    def __escoger_archivo(self):
-        # Buscar archivo a cargar en la biblioteca
-        ruta = tkinter.filedialog.askopenfilename(filetypes=[("Archivos CSV", "*.csv"),
-                                                               ("Archivos Excel", "*.xlsx"),
-                                                             ("Archivos SQL", "*.db"),
-                                                             ("Todos los archivos", "*.*")])
-        if ruta:
-            if ruta.lower().endswith(".csv"):
-                self.data = leer_csv(ruta)
-            elif ruta.lower().endswith(".xlsx"):
-                self.data = leer_xlsx(ruta)
-            elif ruta.lower().endswith(".db"):
-                self.data = leer_sql(ruta)
-            else:
-                Ventana_Error("El archivo introducido\n no es válido")
-                return
-    
-            self.et_path.config(text=f"Archivo cargado: {ruta[:50]}...", bg="light blue")
-            self.et_path.pack_configure(side=tkinter.LEFT, padx= 5, pady=15)
-
-            if self.frame_var: #si habia otro archivo escogido antes
-                self.frame_var.destroy()
-            if hasattr(self, 'frame_mod'): #si habia un modelo cargado
-                self.frame_mod.destroy()
-            self.frame_variables()
-        else:
-            Ventana_Error("No se encontró\n ningún archivo")
 
     def frame_variables(self):
         # Crear el segundo frame de la interfaz para seleccionar variables
---
+++ escoger_arch_ventana.py
@@ -0,0 +1,47 @@
+import tkinter 
+#nuestro
+from leer_archivos import *
+from ventanas_auxiliares import Ventana_Error
+
+def escoger_archivo(self):
+    # Crear el primer frame de la ventana
+    self.frame_base = tkinter.Frame(self.ventana)
+    self.frame_base.pack()
+
+    self.boton_escoger = tkinter.Button(self.frame_base, text="Escoger archivo",
+                                        command=lambda:__escoger_archivo(self),
+                                        bg="light grey", width=17, height=0)
+    
+    self.boton_escoger.pack(side=tkinter.LEFT, padx=5, pady=15)
+
+    self.et_path = tkinter.Label(self.frame_base)
+    self.et_path.pack()
+    
+
+def __escoger_archivo(self):
+    # Buscar archivo a cargar en la biblioteca
+    ruta = tkinter.filedialog.askopenfilename(filetypes=[("Archivos CSV", "*.csv"),
+                                                            ("Archivos Excel", "*.xlsx"),
+                                                            ("Archivos SQL", "*.db"),
+                                                            ("Todos los archivos", "*.*")])
+    if ruta:
+        if ruta.lower().endswith(".csv"):
+            self.data = leer_csv(ruta)
+        elif ruta.lower().endswith(".xlsx"):
+            self.data = leer_xlsx(ruta)
+        elif ruta.lower().endswith(".db"):
+            self.data = leer_sql(ruta)
+        else:
+            Ventana_Error("El archivo introducido\n no es válido")
+            return
+
+        self.et_path.config(text=f"Archivo cargado: {ruta[:50]}...", bg="light blue")
+        self.et_path.pack_configure(side=tkinter.LEFT, padx= 5, pady=15)
+
+        if self.frame_var: #si habia otro archivo escogido antes
+            self.frame_var.destroy()
+        if hasattr(self, 'frame_mod'): #si habia un modelo cargado
+            self.frame_mod.destroy()
+        self.frame_variables()
+    else:
+        Ventana_Error("No se encontró\n ningún archivo")
